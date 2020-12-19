1b0fecd8ef05e8779d29254d169eb7c64ea9fc7f
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-12-19 12:08:30 -0300

    added dotenv to project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenvConfig from 'dotenv';
 
 const app = express();
+app.use(cors());
+
+dotenvConfig.config();
+const mongoDB = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@myshop.sjqif.mongodb.net/test`;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
 
 app.get('/', (req, res) => {
-    res.send(`
-        <h1>Hello, World!</h1>
-    `)
+
 })
 
 
