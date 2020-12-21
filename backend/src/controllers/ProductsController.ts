f326e224c27109b382b31a8258dadbcc21aed5fd
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-12-21 18:03:45 -0300

    adjusted my controller with crud routes

--- backend/src/controllers/ProductsController.ts
+++ backend/src/controllers/ProductsController.ts
@@ -1,52 +1,66 @@
 import { Request, Response } from 'express';
 import db from '../db/config';
-import { Error } from 'mongoose';
 
 import MyProductModel from '../models/ProductSchema';
-import TesteModel from '../models/TestSchema';
 
 const MongoDB = db;
 
-const MyProducts = new MyProductModel({
-    type: 'Eletronic',
-    name: 'Mouse',
-    specs: '10000dpi',
-    price: 150.00
-})
-
-const meuTeste = new TesteModel({
-    type: 'teste',
-    testando: true
-})
-
-const CreateProduct = () => {
-// MyProducts.save(function (err) {
-//     if (err) return new Error('Erro ao salvar!'); 
-//     console.log('Salvo com sucesso!')
-// });
-}
-
 
 export default {
+
     async index(req: Request, res: Response) {
+        const query = req.query;
+        
+        await MyProductModel.find(query)
+        .then(response => res.status(200).send(response))
+        .catch(err => res.status(404).send(err))
+    },
 
-        const PRODUCTS = await MyProductModel.find({})
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
-
-        res.status(201).send(PRODUCTS);
+    async FindByID(request: Request, response: Response) {
+        const { id } = request.query;
+        
+        await MyProductModel.findById({ id })
+        .then(res => response.status(200).send(res))
+        .catch(err => response.status(204).send({ error: err}))
     },
 
     async create(req: Request, res: Response) {
-        res.status(200).send(req.body);
-        meuTeste.save( err => {
-            if(err) return new Error('Erro ao criar!');
-            console.log('Salvo com sucesso!');
-        })
+        const { type, name, specs, price } = req.body;
+        
+        if(!req.query){
+            return res.status(404).send({ message: 'NOT FOUND QUERIES'})
+        }
+
+        const MyProducts = new MyProductModel({ type, name, specs, price });
+
+        await MyProducts.save()
+        .then(response => res.status(201).send(response))
+        .catch(err => res.status(409).send(err));
+    
+    },
+
+    async update(req: Request, res: Response){
+        const { id } = req.query;
+
+        if(!req.query){
+            return res.status(400).send(res);
+        }
+
+        await MyProductModel.findByIdAndUpdate(id, req.body)
+        .then(response => res.status(200).send(response))
+        .catch(err => res.status(400).send(err))
+        
     },
 
     async delete(req: Request, res: Response){
+        console.log(req.query);
 
+        if(!req.query){
+            return res.status(404).send({ message: 'Query not found!'})
+        }
+
+        await MyProductModel.deleteMany(req.query);
+        return res.status(402).send({ message: 'Doc was deleted successfully'});
     },
 
     async deleteByID(req: Request, res: Response){
@@ -55,27 +69,10 @@ export default {
             return res.status(404).send({ error: 'Not Found!'});
         }
 
-        const ProductToBeDeleted = await MyProductModel.findByIdAndDelete(id);
+        await MyProductModel.findByIdAndDelete(id);
         res.status(202).send({ message: 'Deleted'});
     },
 
-    async deleteMany(req: Request, res: Response){
-        const { type } = req.query;
-
-        if(!type){
-            return res.status(404).send({ error: 'Could not find!'})
-        }
-
-        const ProductsToBeDeleted = await MyProductModel.deleteMany({type});
-        res.status(202).send({ message: 'Successful'});
-    },
-
-    async FindByID(request: Request, response: Response) {
-        const { id } = request.query;
-        
-        const PRODUCTS = await MyProductModel.findById({ _id: id })
-        .then(res => response.status(200).send(res))
-        .catch(err => response.status(204).send(err))
-    },
+    
 
 }
