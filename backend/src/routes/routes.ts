7256b87e9853d14f033173db70eb80606f4bfbdf
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-12-21 18:02:32 -0300

    added new routes and removes others

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -4,15 +4,20 @@ import ProductController from '../controllers/ProductsController';
 const router = Router();
 
 
-
+//Product Router
 router.get('/products', ProductController.index);
 router.get('/products/:id/', ProductController.FindByID);
 
+
 router.post('/products', ProductController.create);
 
+router.put('/products/:id', ProductController.update)
+
 router.delete('/products', ProductController.delete);
-router.delete('/products/:id', ProductController.deleteByID)
-router.delete('/products/:type', ProductController.deleteMany)
+router.delete('/products/:id', ProductController.deleteByID);
+
+router.get('/MyProducts', ProductController.index);
+
 
 
 export default router;
