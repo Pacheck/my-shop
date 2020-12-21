167e16788e18ccf8045be6d813816b68599e15e3
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-12-20 23:43:45 -0300

    created new endpoints

--- backend/src/controllers/ProductsController.ts
+++ backend/src/controllers/ProductsController.ts
@@ -1,35 +1,81 @@
 import { Request, Response } from 'express';
+import db from '../db/config';
+import { Error } from 'mongoose';
+
 import MyProductModel from '../models/ProductSchema';
+import TesteModel from '../models/TestSchema';
+
+const MongoDB = db;
 
 const MyProducts = new MyProductModel({
-    type: 'Food',
-    name: 'Hot-Dog',
-    specs: 'Bread with sausage',
-    price: 5.00
+    type: 'Eletronic',
+    name: 'Mouse',
+    specs: '10000dpi',
+    price: 150.00
 })
 
-const products = [
-    {
-        type: 'eletronic',
-        name: 'LCD',
-        specs: '28"',
-        price: '2000R$'
-    },
-    {
-        type: 'eletronic',
-        name: 'Redragon Kumara 522',
-        specs: 'Outemu Blue',
-        price: '250R$'
-    }
-]
+const meuTeste = new TesteModel({
+    type: 'teste',
+    testando: true
+})
+
+const CreateProduct = () => {
+// MyProducts.save(function (err) {
+//     if (err) return new Error('Erro ao salvar!'); 
+//     console.log('Salvo com sucesso!')
+// });
+}
+
 
 export default {
     async index(req: Request, res: Response) {
-        res.status(200).send(MyProducts);
+
+        const PRODUCTS = await MyProductModel.find({})
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+
+        res.status(201).send(PRODUCTS);
     },
 
-    async CUCK(req: Request, res: Response) {
-        res.status(200).send({ message: 'All worked well!'});
-        console.log(req.body);
-    }
+    async create(req: Request, res: Response) {
+        res.status(200).send(req.body);
+        meuTeste.save( err => {
+            if(err) return new Error('Erro ao criar!');
+            console.log('Salvo com sucesso!');
+        })
+    },
+
+    async delete(req: Request, res: Response){
+
+    },
+
+    async deleteByID(req: Request, res: Response){
+        const { id } = req.query;
+        if(!id){
+            return res.status(404).send({ error: 'Not Found!'});
+        }
+
+        const ProductToBeDeleted = await MyProductModel.findByIdAndDelete(id);
+        res.status(202).send({ message: 'Deleted'});
+    },
+
+    async deleteMany(req: Request, res: Response){
+        const { type } = req.query;
+
+        if(!type){
+            return res.status(404).send({ error: 'Could not find!'})
+        }
+
+        const ProductsToBeDeleted = await MyProductModel.deleteMany({type});
+        res.status(202).send({ message: 'Successful'});
+    },
+
+    async FindByID(request: Request, response: Response) {
+        const { id } = request.query;
+        
+        const PRODUCTS = await MyProductModel.findById({ _id: id })
+        .then(res => response.status(200).send(res))
+        .catch(err => response.status(204).send(err))
+    },
+
 }
