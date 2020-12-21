0dc00387f2caf4c51519c2e05738c672e0724448
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-12-20 23:42:59 -0300

    create and update routes

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -5,9 +5,14 @@ const router = Router();
 
 
 
-router.get('/products', ProductController.index)
+router.get('/products', ProductController.index);
+router.get('/products/:id/', ProductController.FindByID);
 
-router.post('/products', ProductController.CUCK)
+router.post('/products', ProductController.create);
+
+router.delete('/products', ProductController.delete);
+router.delete('/products/:id', ProductController.deleteByID)
+router.delete('/products/:type', ProductController.deleteMany)
 
 
 export default router;
