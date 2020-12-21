2f3dcaa4a32cde4961a4a2a9130920755d8f1005
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-12-21 18:03:20 -0300

    added new property to use a method findByIDAndDelete

--- backend/src/db/config.ts
+++ backend/src/db/config.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 import dotenvConfig from 'dotenv';
 
 dotenvConfig.config();
-const mongoDB = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@myshop.sjqif.mongodb.net/test`;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoDB = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@myshop.sjqif.mongodb.net/MyShop`;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 const db = mongoose.connection;
 
